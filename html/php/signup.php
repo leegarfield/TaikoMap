d1380831a3c3b2dc7a5e22cb85cc14b4473f700f
leegarfield <759634382lee@gmail.com>
2020-01-06 03:27:11 +0800

    login email check
    现在新用户注册需要提供有效的验证邮箱

--- html/php/signup.php
+++ html/php/signup.php
@@ -10,65 +10,133 @@ $cookieExpTime = $time + 2678400;
 
 // var_dump($_POST);
 
-if(!$_POST['nickname']){
-  header("Location: ".HOST."/template/error.php?msg=".urlencode("请填写昵称"));
+//mode selection
+if(isset($_POST['email_addr'])){
+  //send verlify email and store all shit into session
+  if(!$_POST['nickname']){
+    echo("请填写昵称");
+    exit;
+  }
+  if(!$_POST['password']){
+    echo("请填写密码");
+    exit;
+  }
+  if(!$_POST['password2']){
+    echo("请确认密码");
+    exit;
+  }
+  if(!$_POST['email_addr']){
+    echo("请输入邮箱");
+    exit;
+  }
+  if($_POST['password2']!=$_POST['password']){
+    echo("密码不一致");
+    exit;
+  }
+  if($_POST['check']!='true'){
+    echo("请同意用户协议");
+    exit;
+  }
+  
+  $arr['nickname'] = addslashes($_POST['nickname']);
+  $arr['email'] = addslashes($_POST['email_addr']);
+  
+  //prevent overlap
+  $table = 'player_table';
+  $start = connect();
+  $sql = 'select nickname, email from player_table';
+  $result = fetchAll($sql, $start);
+  foreach($result as $key => $val){
+    $namelist[] = $val['nickname'];
+    $emaillist[] = $val['email'];
+  }
+  if (in_array($_POST['nickname'], $namelist)){
+    echo("昵称已存在");
+    exit;
+  }
+  if (in_array($_POST['email_addr'], $emaillist)){
+    echo("请换个邮箱");
+    exit;
+  }
+  
+  
+  //password enrc
+  $arr['password'] = passwordCrypt($_POST['password']);
+  
+  $arr['token'] = passwordCrypt($time.$arr['nickname'].'this is a rendom char!');
+  $arr['login_time'] = $time;
+  
+  //store all the shit into session
+  $_SESSION['arr'] = $arr;
+
+  //send mail
+
+  //generate code
+  $code['f'] = rand(1000,9999);
+  $code['e'] = rand(1000,9999);
+  $code['n'] = $code['f'].  $code['e'];
+  //store code into session
+  $_SESSION['verlify'] = $code['n'];
+  //prevent spam
+  if(isset($_SESSION['email_send_time']) && $time - $_SESSION['email_send_time'] <= 120){
+    $timespace = 120 - $time + $_SESSION['email_send_time'];
+    echo("请等待". $timespace ."秒再次发送，若未找到邮件，请尝试检查邮件垃圾箱");
+    exit;
+  }else{
+    $_SESSION['email_send_time'] = $time;
+  }
+  //mail construt
+  $to = $arr['email'];
+  $subject = "TaikoMap太鼓地图注册邮箱验证";
+  $body = '
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+您申请在TaikMap太鼓地图（<a href="https://taikomap.cn">https://taikomap.cn</a>）注册<br />
+您的校验码是：
+<pre style="white-space: pre-wrap;word-wrap: break-word;background-color: #eee;padding:10px;">'.$code['n'].'</pre><br />
+如果您并未在TaikMap太鼓地图申请注册，请忽略此邮件<br /><br />
+<p style="text-align: right;">TaikoMap太鼓地图</p>
+<p style="text-align: right;">'.date('y年m月d日',time()).'</p>
+</html>';
+  email($to, $subject, $body, $altbody = '');
+  
+  echo "success";
+  // echo $code['f']. ' '.  $code['e'] . ' [' .  $code['n'] . '] time: ' . $_SESSION['email_send_time'];
+
+}else if(isset($_POST['token'])){
+  //check token
+  if($_POST['token'] == $_SESSION['verlify']){
+    //nomarl signup setup
+    $start = connect();
+    $arr = $_SESSION['arr'];
+  
+    //insert into sql
+    $sql = insert('player_table', $arr, $start);
+    
+    $sql = 'select NUM from player_table where nickname = "'.$arr['nickname'].'"';
+    $result = fetchOne($sql, $start);
+    
+    //set cookie & session
+    setcookie('token',$arr['token'],$cookieExpTime, '/');
+    setcookie('num',$result['NUM'],$cookieExpTime, '/');
+    $_SESSION['checked'] = true;
+    $_SESSION['logged'] = true;
+    $_SESSION['verlify'] = null;
+    $_SESSION['arr'] = null;
+    
+    //log
+    logToDatabase($start, $result['NUM'], 'signup', '', '', '', '【'.$arr['nickname'].'】-ip：【'.$_SERVER['REMOTE_ADDR'].'】');
+    //return
+    echo 'success';
+    
+  }else{
+    echo "校验码错误";
+  }
+
+}else{
+  echo("请输入邮箱");
   exit;
 }
-if(!$_POST['password']){
-  header("Location: ".HOST."/template/error.php?msg=".urlencode("请填写密码"));
-  exit;
-}
-if(!$_POST['password2']){
-  header("Location: ".HOST."/template/error.php?msg=".urlencode("请确认密码"));
-  exit;
-}
-if($_POST['password2']!=$_POST['password']){
-  header("Location: ".HOST."/template/error.php?msg=".urlencode("密码不一致"));
-  exit;
-}
-if(!$_POST['check']){
-  header("Location: ".HOST."/template/error.php?msg=".urlencode("请同意用户协议"));
-  exit;
-}
-
-$arr['nickname'] = addslashes($_POST['nickname']);
-$arr['email'] = addslashes($_POST['email']);
-
-//search for overlap
-$table = 'player_table';
-$start = connect();
-$sql = 'select nickname from player_table';
-$result = fetchAll($sql, $start);
-foreach($result as $key => $val){
-  $namelist[] = $val['nickname'];
-}
-if (in_array($_POST['nickname'], $namelist)){
-  header("Location: ".HOST."/template/error.php?msg=".urlencode("昵称已存在"));
-  exit;
-}
-
-
-//password enrc
-$arr['password'] = passwordCrypt($_POST['password']);
-
-$arr['token'] = passwordCrypt($time.$arr['nickname'].'this is a rendom char!');
-$arr['login_time'] = $time;
-
-// var_dump($arr);
-
-$sql = insert($table, $arr, $start);
-$_SESSION['logged'] = true;
-
-$sql = 'select NUM from player_table where nickname = "'.$arr['nickname'].'"';
-$result = fetchOne($sql, $start);
-
-setcookie('token',$arr['token'],$cookieExpTime, '/');
-setcookie('num',$result['NUM'],$cookieExpTime, '/');
-
-
-logToDatabase($start, $result['NUM'], 'signup', '', '', '', '【'.$arr['nickname'].'】-ip：【'.$_SERVER['REMOTE_ADDR'].'】');
-
-header("Location: ".HOST);
-
-
-
--- html/template/loginSingupModal.php
+++ html/template/loginSingupModal.php
@@ -9,7 +9,7 @@ if (isset($acc['NUM'])){
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">Login</h4>
+            <h4 class="modal-title">登录</h4>
           </div>
           <div class="modal-body">
             <form id='loginForm' action="/php/login.php" method="post">
@@ -34,36 +34,117 @@ if (isset($acc['NUM'])){
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">Signup</h4>
+            <h4 class="modal-title">注册</h4>
           </div>
           <div class="modal-body">
-            <form id='signupForm' action="/php/signup.php" method="post">
+            <div class="alert alert-warning"><strong id='signupModal_alert'>带*号的为必填项</strong></div>
+            <form id='signupForm' action="" method="post">
               <div class="form-group">
-                <label for="InputNickname2">输入昵称</label>
+                <label for="InputNickname2">*输入昵称</label>
                 <input name="nickname" type="text" class="form-control" id="InputNickname2" placeholder="nickname">
               </div>
               <div class="form-group">
-                <label for="InputPassword2">输入密码</label>
+                <label for="InputPassword2">*输入密码</label>
                 <input name="password" type="password" class="form-control" id="InputPassword2" placeholder="Password">
               </div>
               <div class="form-group">
-                <label for="ConfirmPassword1">确认密码</label>
+                <label for="ConfirmPassword1">*确认密码</label>
                 <input name="password2" type="password" class="form-control" id="ConfirmPassword1" placeholder="Password">
               </div>
               <div class="form-group">
-                <label for="InputEmail1">(可选)设置一个邮箱用于密码找回(暂不可用)</label>
-                <input name="email" type="email" class="form-control" id="InputEmail1" placeholder="abc@example.com">
+                <label for="InputEmail1">*设置一个邮箱</label>
+                <input name="email_addr" type="email" class="form-control" id="InputEmail1" placeholder="abc@example.com">
               </div>
               <div class="checkbox">
                 <label>
-                  <input name="check" type="checkbox"> 确认遵守目前还不存在的用户协议
+                  <input name="check" type="checkbox" id='CheckBox1'> 确认遵守目前还不存在的用户协议
                 </label>
               </div>
-              <button type="submit" class="btn btn-primary">注册</button>
             </form>
+            <div id='verlifyForm' style="display: none;">
+              <form action="" method="post">
+                <div class="form-group">
+                  <label for="InputCode">请前往邮箱，输入获取到的校验码</label>
+                  <input name="token" type="number" class="form-control" id="InputToken1" placeholder="123456">
+                </div>
+              </form>
+              <button type="submit" class="btn btn-success" id="signupModal_btn2">提交校验码</button>
+              <button type="submit" class="btn btn-warning" id="signupModal_btn3">返回修改邮箱</button>
+            </div>
+            <br />
+            <button type="submit" class="btn btn-block btn-primary" id="signupModal_btn">发送验证邮件</button>
+            <button style="display: none;" type="submit" class="btn btn-block btn-warning" id="signupModal_btn4">返回填写校验码</button>
           </div>
         </div>
       </div>
+      <script>
+        $(document).ready(function(){
+          $('#signupModal_btn').click(function(){
+            $('#signupModal_btn').text("发送中。。。");
+            var post = {
+              nickname : $('#InputNickname2').val(),
+              password : $('#InputPassword2').val(),
+              password2 : $('#ConfirmPassword1').val(),
+              email_addr : $('#InputEmail1').val(),
+              check : $('#CheckBox1').prop("checked")
+            };
+
+            unSyncPost(post, '/php/signup.php', function(data){
+              if(data == 'success'){
+                $('#signupModal_btn').text("再次发送");
+                $('#signupModal_alert').text("发送成功！");
+                $('#signupModal_alert').parent().removeClass('alert-warning');
+                $('#signupModal_alert').parent().removeClass('alert-danger');
+                $('#signupModal_alert').parent().addClass('alert-success');
+                $('#signupForm').hide();
+                $('#signupModal_btn4').hide();
+                $('#verlifyForm').show();
+              }else{
+                $('#signupModal_alert').text(data);
+                $('#signupModal_alert').parent().removeClass('alert-warning');
+                $('#signupModal_alert').parent().removeClass('alert-success');
+                $('#signupModal_alert').parent().addClass('alert-danger');
+                $('#signupModal_btn').text("发送验证邮件");
+              }
+            });
+          });
+
+          $('#signupModal_btn2').click(function(){
+            var post = {
+              token: $('#InputToken1').val()
+            };
+
+            unSyncPost(post, '/php/signup.php', function(data){
+              if(data == 'success'){
+                $('#signupModal_alert').text('注册成功！即将自动<a href="/">刷新页面</a>');
+                $('#signupModal_alert').parent().removeClass('alert-warning');
+                $('#signupModal_alert').parent().removeClass('alert-danger');
+                $('#signupModal_alert').parent().addClass('alert-success');
+                $('#verlifyForm').hide();
+                setTimeout(function(){
+                  window.location.reload(true);
+                }, 1000)
+              }else{
+                $('#signupModal_alert').text(data);
+                $('#signupModal_alert').parent().removeClass('alert-warning');
+                $('#signupModal_alert').parent().removeClass('alert-success');
+                $('#signupModal_alert').parent().addClass('alert-danger');
+              }
+            });
+          });
+
+          $('#signupModal_btn3').click(function(){
+            $('#signupForm').show();
+            $('#signupModal_btn4').show();
+            $('#verlifyForm').hide();
+          });
+          $('#signupModal_btn4').click(function(){
+            $('#signupForm').hide();
+            $('#signupModal_btn4').hide();
+            $('#verlifyForm').show();
+          });
+        });
+      </script>
     </div>
 <?php
 }
