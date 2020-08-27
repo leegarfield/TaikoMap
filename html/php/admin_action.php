f6612668cdd5fea2df708fc6b2c347458aecaa53
leegarfield <759634382lee@gmail.com>
2020-08-27 22:03:17 +0800

    add one admin action
    one click to update html template

--- html/php/admin_action.php
+++ html/php/admin_action.php
@@ -1,11 +1,30 @@
 <?php
 //debug信息输出
-// ini_set("display_errors", "On");
-// error_reporting(E_ALL | E_STRICT);
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
 
-var_dump($_GET['a']);
+$time = time();
+require_once ('../include.php');
 
-switch($_GET['a']){
-    case 'regen_html':
-    break;
+//login check
+$acc = logincheck();
+
+//admin check
+if(isset($acc['token'])&$acc['info_is_admin']){
+
+    // var_dump($_GET['a']);
+    
+    switch($_GET['a']){
+        case 'regen_html':
+        require_once ('template.php');
+        //update citylist;
+        $pageOp = new PageOpreate;
+        $pageOp->updateAll();
+    }
+    
+    header("Location: ".HOST);
+
+}else{
+    header("Location: ".HOST."/error/404.html");
+    exit;
 }
